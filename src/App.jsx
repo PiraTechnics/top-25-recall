9c225d094c01f9983ba999f37c396968294d28cc
PiraTechnics <devin1younge@gmail.com>
2023-09-17 17:35:21 -0700

    API, basic display, order shuffling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [topTeams, setTopTeams] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const handleClick = () => {
+		const newList = [...topTeams];
+		const shuffledList = shuffle(newList);
+		setTopTeams(shuffledList);
+	};
+
+	const shuffle = (array) => {
+		for (let i = array.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
+		}
+		return array;
+	};
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await fetch(
+				"http://site.api.espn.com/apis/site/v2/sports/football/college-football/rankings"
+			);
+
+			const teamData = await response.json();
+
+			console.log(teamData.rankings[0].headline);
+			console.log(teamData.rankings[0].ranks);
+
+			setTopTeams(teamData.rankings[0].ranks);
+		};
+
+		fetchData().catch((err) => console.log(err.message));
+	}, []);
+
+	const top25 = topTeams.map((team) => (
+		<Col key={team.team.id} xs={6} sm={4} md={3} lg={2} className="my-2">
+			<Image thumbnail src={team.team.logo} />
+			<p>#{team.current}</p>
+		</Col>
+	));
+
+	return (
+		<Container fluid>
+			<Row>{top25}</Row>
+			<Row>
+				<button onClick={handleClick}>Shuffle</button>
+			</Row>
+		</Container>
+	);
 }
 
-export default App
+export default App;
